fcb0f6f0486cb264fe766bde1f7e2c46963c6135
os <omg201050@gmail.com>
2018-02-07 17:47:41 +0300

    10

--- js/app.js
+++ js/app.js
@@ -1,135 +1,40 @@
-// Global Variables
-var map, clientID, clientSecret;
+//javascript
+function initMap() {
+    // Map options
+    var options = {
+        zoom: 5,
+        center: {lat: 24.7136, lng: 46.6753}
+    }
 
-function AppViewModel() {
-    var self = this;
+    // new map
+    var map = new google.maps.Map(document.getElementById('map'), options);
 
-    this.searchOption = ko.observable("");
-    this.markers = [];
+    var markers = [
+        {
+            coords: {lat: 24.7136, lng: 46.6753},
+            content: '<h1>osama</h1>'
+        }
+    ];
 
-    // This function populates the infowindow when the marker is clicked. We'll only allow
-    // one infowindow which will open at the marker that is clicked, and populate based
-    // on that markers position.
-    this.populateInfoWindow = function(marker, infowindow) {
-        if (infowindow.marker != marker) {
-            infowindow.setContent('');
-            infowindow.marker = marker;
-            // Foursquare API Client
-            clientID = "2G4BOAVMDDTBVKZOU0WI0IBXSQOCMDTIOWZCKXS4XO1RAC0R";
-            clientSecret =
-                "3UZMRJ1XEB1WDHZROFUCCIGDJCFMWPVRG5J4FFDWVDNHEV4K";
-            // URL for Foursquare API
-            var apiUrl = 'https://api.foursquare.com/v2/venues/search?ll=' +
-                marker.lat + ',' + marker.lng + '&client_id=' + clientID +
-                '&client_secret=' + clientSecret + '&query=' + marker.title +
-                '&v=20170708' + '&m=foursquare';
-            // Foursquare API
-            $.getJSON(apiUrl).done(function(marker) {
-                var response = marker.response.venues[0];
-                self.street = response.location.formattedAddress[0];
-                self.city = response.location.formattedAddress[1];
-                self.zip = response.location.formattedAddress[3];
-                self.country = response.location.formattedAddress[4];
-                self.category = response.categories[0].shortName;
+    for (var i = 0; i < markers.length; i++) {
+        addMarker(markers[i]);
+    }
 
-                self.htmlContentFoursquare =
-                    '<h5 class="iw_subtitle">(' + self.category +
-                    ')</h5>' + '<div>' +
-                    '<h6 class="iw_address_title"> Address: </h6>' +
-                    '<p class="iw_address">' + self.street + '</p>' +
-                    '<p class="iw_address">' + self.city + '</p>' +
-                    '<p class="iw_address">' + self.zip + '</p>' +
-                    '<p class="iw_address">' + self.country +
-                    '</p>' + '</div>' + '</div>';
-
-                infowindow.setContent(self.htmlContent + self.htmlContentFoursquare);
-            }).fail(function() {
-                // Send alert
-                alert(
-                    "There was an issue loading the Foursquare API. Please refresh your page to try again."
-                );
+    // Add marker function
+    function addMarker(props) {
+        var marker = new google.maps.Marker({
+            position: props.coords,
+            map: map
+        });
+        if (props.content) {
+            // info
+            var infoWindow = new google.maps.InfoWindow({
+                content: props.content
+            });
+            marker.addListener('click', function () {
+                infoWindow.open(map, marker);
             });
 
-            this.htmlContent = '<div>' + '<h4 class="iw_title">' + marker.title +
-                '</h4>';
-
-            infowindow.open(map, marker);
-
-            infowindow.addListener('closeclick', function() {
-                infowindow.marker = null;
-            });
         }
-    };
-
-    this.populateAndBounceMarker = function() {
-        self.populateInfoWindow(this, self.largeInfoWindow);
-        this.setAnimation(google.maps.Animation.BOUNCE);
-        setTimeout((function() {
-            this.setAnimation(null);
-        }).bind(this), 1400);
-    };
-
-    this.initMap = function() {
-        var mapCanvas = document.getElementById('map');
-        var mapOptions = {
-            center: new google.maps.LatLng(51.4980479, -0.0105351),
-            zoom: 15,
-            styles: styles
-        };
-        // Constructor creates a new map - only center and zoom are required.
-        map = new google.maps.Map(mapCanvas, mapOptions);
-
-        // Set InfoWindow
-        this.largeInfoWindow = new google.maps.InfoWindow();
-        for (var i = 0; i < myLocations.length; i++) {
-            this.markerTitle = myLocations[i].title;
-            this.markerLat = myLocations[i].lat;
-            this.markerLng = myLocations[i].lng;
-            // Google Maps marker setup
-            this.marker = new google.maps.Marker({
-                map: map,
-                position: {
-                    lat: this.markerLat,
-                    lng: this.markerLng
-                },
-                title: this.markerTitle,
-                lat: this.markerLat,
-                lng: this.markerLng,
-                id: i,
-                animation: google.maps.Animation.DROP
-            });
-            this.marker.setMap(map);
-            this.markers.push(this.marker);
-            this.marker.addListener('click', self.populateAndBounceMarker);
-        }
-    };
-
-    this.initMap();
-
-    // This block appends our locations to a list using data-bind
-    // It also serves to make the filter work
-    this.myLocationsFilter = ko.computed(function() {
-        var result = [];
-        for (var i = 0; i < this.markers.length; i++) {
-            var markerLocation = this.markers[i];
-            if (markerLocation.title.toLowerCase().includes(this.searchOption()
-                    .toLowerCase())) {
-                result.push(markerLocation);
-                this.markers[i].setVisible(true);
-            } else {
-                this.markers[i].setVisible(false);
-            }
-        }
-        return result;
-    }, this);
-}
-
-googleError = function googleError() {
-    alert(
-        'Oops. Google Maps did not load. Please refresh the page and try again!'
-    );
-};
-
-function startApp() {
-    ko.applyBindings(new AppViewModel());
+    }
 }
