7112c940063dfd1e516f19bbfd0b0c9c5b27adcb
os <omg201050@gmail.com>
2018-02-10 10:12:53 +0300

    15

--- js/app.js
+++ js/app.js
@@ -1,182 +1,224 @@
-var map;
+// declaring global variables
+var map, infoWindow, bounds;
 
-// class to instantiate all of the places
-function place(name, coordin, img, visible, catagory) {
-
-    this.name = ko.observable(name);
-    this.coordin = ko.observable(coordin);
-    this.img = ko.observable(img);
-    this.visible = ko.observable(visible);
-    // google maps marker
-    this.markerObject = null;
-}
-
-
-function viewModel() {
-
-    // Smart array of all the places
-    this.places = ko.observableArray([
-        new place(
-            "King Saud University", {
-                lat: 24.7224,
-                lng: 46.6271
-            },
-            "img/Ksu.jpg",
-            true,
-            "University"),
-        new place(
-            "Kingdom Centre", {
-                lat: 24.7111837,
-                lng: 46.67340100000001
-            },
-            "img/Kingdom_Center_.jpg",
-            true,
-            "mall hotel office"),
-        new place(
-            "KAFD World Trade Center", {
-                lat: 24.76195599999999,
-                lng: 46.64043370000002
-            },
-            "img/1stGrade-Panora Park- Financial Plaza (22)PhotoGala.jpg",
-            true,
-            "mall hotel offices office company"),
-        new place(
-            "Al Faisaliyah Center", {
-                lat: 24.6905765,
-                lng: 46.68509700000004
-            },
-            "img/faisaleh.jpg",
-            true,
-            "mall hotel office"),
-        new place(
-            "Royal commission", {
-                lat: 24.684340,
-                lng: 46.637900
-            },
-            "img/Royal.jpg",
-            true,
-            "offices company")
-    ]);
-
-    // getting the input from the input box
-    this.filterString = ko.observable('');
-
-    this.submit = function () {
-        var filterString = this.filterString();
-        for (var i = 0; i < this.places().length; i++) {
-            var name = this.places()[i].name();
-            this.places()[i].visible((name.indexOf(filterString) >= 0 ) ? true : false);
+/* Model Data */
+var locations = [
+    {
+        title: 'Nakheel Tower',
+        location: {
+            lat: 24.7488774,
+            lng: 46.65273419999994
         }
-        //doing/undoing based on marker visible value
-        undoMarker(filterString);
-        infoWindow.close();
-    };
-
-    this.listClick = function (location) {
-        //loop in the markers array to match marker with location
-        for (var i = 0; i < markers.length; i++) {
-            if (markers[i].loc == location) {
-                markers[i].setAnimation(4);
-                popUpInfoWindow(location);
-            }
+    },
+    {
+        title: 'Kingdom Centre',
+        location: {
+            lat: 24.7114,
+            lng:  46.6744
         }
-    };
-
-}
-
-
-// erasing markers once filtered
-function undoMarker(filterString) {
-    for (var i = 0; i < markers.length; i++) {
-        var name = markers[i].loc.name();
-        if (name.indexOf(filterString) >= 0) {
-            markers[i].setVisible(true);
-        } else {
-            markers[i].setVisible(false);
+    },
+    {
+        title: 'KAFD World Trade Center',
+        location: {
+            lat: 24.76195599999999,
+            lng: 46.64043370000002
+        }
+    },
+    {
+        title: 'Al Faisaliyah Center',
+        location: {
+            lat: 24.6905765,
+            lng: 46.6850970
+        }
+    },
+    {
+        title: 'Burj Rafal',
+        location: {
+            lat: 24.7925009,
+            lng: 46.632335799999964
         }
     }
-}
+];
 
-//knockout binding with index.html
-ko.applyBindings(new viewModel());
-
-// google map error function
-function mapError() {
-    alert('Google Maps is temporarily unavailable. Try again later');
-}
-
-var markers = [];
-var vm = ko.dataFor(document.body);
-
-
+// google maps init
 function initMap() {
-    infoWindow = new google.maps.InfoWindow({
-        content: 'emptyStringHypothatically'
-    });
-    var mapElemnt = document.getElementById('map');
-    var uluru = {
-        lat: 24.7224,
-        lng: 46.6271
+    var Riyadh = {
+        lat: 24.7136,
+        lng: 46.6753
     };
-    //set where we want the map to be
-    map = new google.maps.Map(mapElemnt, {
-        center: uluru,
-        zoom: 12,
-        mapTypeControlOptions: {
-            position: google.maps.ControlPosition.LEFT_BOTTOM
+    map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 8,
+        center: Riyadh
+    });
+
+    infoWindow = new google.maps.InfoWindow();
+
+    bounds = new google.maps.LatLngBounds();
+
+    ko.applyBindings(new ViewModel());
+}
+
+// handle map error
+function googleMapsError() {
+    alert('An error occurred with Google Maps!');
+}
+
+/* Location Model */
+var LocationMarker = function(data) {
+    var self = this;
+
+    this.title = data.title;
+    this.position = data.location;
+    this.visible = ko.observable(true);
+
+    // Style the markers a bit. This will be our listing marker icon.
+    var defaultIcon = makeMarkerIcon('6C6B6B');
+
+    //  Change color when the user hover the marker.
+    var changeColor = makeMarkerIcon('007BFF');
+
+    // Create a marker per location, and put into markers array
+    this.marker = new google.maps.Marker({
+        position: this.position,
+        title: this.title,
+        animation: google.maps.Animation.DROP,
+        icon: defaultIcon
+    });
+    // set marker and extend bounds (showListings)
+    self.filterMarkers = ko.computed(function () {
+        if(self.visible() === true) {
+            self.marker.setMap(map);
+            bounds.extend(self.marker.position);
+            map.fitBounds(bounds);
+        } else {
+            self.marker.setMap(null);
         }
     });
-    //creating markers for every place
-    for (var i = 0; i < vm.places().length; i++) {
-        var loc = vm.places()[i];
-        var marker = new google.maps.Marker({
-            position: loc.coordin(),
-            map: map,
-            loc: loc,
-            animation: null
+
+    // Make an onclick even to open an indowindow at each marker
+    this.marker.addListener('click', function() {
+        populateInfoWindow(this, self.street, self.city, self.phone, infoWindow);
+        toggleBounce(this);
+        map.panTo(this.getPosition());
+    });
+
+    // Two event listeners - one for mouseover and another for mouseout to change the colors back.
+    this.marker.addListener('mouseover', function() {
+        this.setIcon(changeColor);
+    });
+    this.marker.addListener('mouseout', function() {
+        this.setIcon(defaultIcon);
+    });
+
+    // show item info when selected from list
+    this.show = function(location) {
+        google.maps.event.trigger(self.marker, 'click');
+    };
+
+    // creates bounce effect when item selected
+    this.bounce = function(place) {
+        google.maps.event.trigger(self.marker, 'click');
+    };
+};
+
+/* View Model */
+var ViewModel = function() {
+    var self = this;
+
+    this.searchItem = ko.observable('');
+
+    this.mapList = ko.observableArray([]);
+
+    // add location markers for each location
+    locations.forEach(function(location) {
+        self.mapList.push( new LocationMarker(location) );
+    });
+
+    // locations viewed on map
+    this.locationList = ko.computed(function() {
+        var searchFilter = self.searchItem().toLowerCase();
+        if (searchFilter) {
+            return ko.utils.arrayFilter(self.mapList(), function(location) {
+                var str = location.title.toLowerCase();
+                var result = str.includes(searchFilter);
+                location.visible(result);
+                return result;
+            });
+        }
+        self.mapList().forEach(function(location) {
+            location.visible(true);
         });
-        loc.markerObject = marker;
-        marker.addListener('click', onClickMarker); // when a marker is clicked run the function that animates
-        markers.push(marker);
+        return self.mapList();
+    }, self);
+};
+
+// This function populates the infowindow when the marker is clicked. We'll only allow
+// one infowindow which will open at the marker that is clicked, and populate based on that markers position.
+function populateInfoWindow(marker, street, city, phone, infowindow) {
+    // Check to make sure the infowindow is not already opened on this marker.
+    if (infowindow.marker != marker) {
+        // Clear the infowindow content to give the streetview time to load.
+        infowindow.setContent('');
+        infowindow.marker = marker;
+
+        // Make sure the marker property is cleared if the infowindow is closed.
+        infowindow.addListener('closeclick', function() {
+            infowindow.marker = null;
+        });
+        var streetViewService = new google.maps.StreetViewService();
+        var radius = 40;
+
+        var windowContent = '<h4>' + marker.title + '</h4>' +
+            '<p>' + street + "<br>" + city + '<br>' + phone + "</p>";
+
+        //if the status is OK, which means the pano was found, compute the
+        // position of the street view image, then calculate the heading, then get a
+        // panorama from that and set the options.
+        var getStreetView = function (data, status) {
+            if (status == google.maps.StreetViewStatus.OK) {
+                var nearStreetViewLocation = data.location.latLng;
+                var heading = google.maps.geometry.spherical.computeHeading(
+                    nearStreetViewLocation, marker.position);
+                infowindow.setContent(windowContent + '<div id="pano"></div>');
+                var panoramaOptions = {
+                    position: nearStreetViewLocation,
+                    pov: {
+                        heading: heading
+                    }
+                };
+                var panorama = new google.maps.StreetViewPanorama(document.getElementById('pano'), panoramaOptions);
+            } else {
+                infowindow.setContent(windowContent + '<div style="color: red">No Street View Found</div>');
+            }
+        };
+        // Use street view service to get the closest street view image with radius 50.
+        streetViewService.getPanoramaByLocation(marker.position, radius, getStreetView);
+        // Open the infowindow on the correct marker.
+        infowindow.open(map, marker);
     }
-
 }
 
-// a function that animates what happens when a marker is clicked
-function onClickMarker() {
-    this.setAnimation(4);
-    popUpInfoWindow(this.loc);
+function toggleBounce(marker) {
+    if (marker.getAnimation() !== null) {
+        marker.setAnimation(null);
+    } else {
+        marker.setAnimation(google.maps.Animation.BOUNCE);
+        setTimeout(function() {
+            marker.setAnimation(null);
+        }, 1400);
+    }
 }
 
-var infoWindow = null;
-
-function popUpInfoWindow(loc) {
-    var name = loc.name();
-    var img = loc.img();
-
-    infoWindow.close();
-    // in case of WIKI API error
-    var wikiError = setTimeout(function () {
-        //error message
-        infoWindow.setContent("<p>ERROR !! wiki content out of service</p>");
-        infoWindow.open(map, loc.markerObject);
-    }, 5000);
-
-    // wiki API
-    $.ajax({
-        dataType: "jsonp",
-        url: "https://en.wikipedia.org/w/api.php?action=query&prop=extracts&format=json&exintro=&titles="+name,
-        success: function (data) {
-            var Article = data.query.pages;
-            // we need articles first key so
-            for (var firstKey in Article)
-                break;
-            // to get the first paragraph of the Article
-            var extract = Article[firstKey].extract;
-            //display the paragraph above a marker
-            infoWindow.setContent('<div class="info"><div class="info-text"><p class="title">' + name + '</p><p class="address">' + extract + '</p></div><div class="bus-img"><img src="' + img + '" alt="place img"></div>');
-            infoWindow.open(map, loc.markerObject);
-            clearTimeout(wikiError);
-        }
-    });
-} //end popup
+// This function takes in a COLOR, and then creates a new marker
+// icon of that color. The icon will be 21 px wide by 34 high, have an origin
+// of 0, 0 and be anchored at 10, 34).
+function makeMarkerIcon(markerColor) {
+    var markerImage = new google.maps.MarkerImage(
+        'http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|' + markerColor +
+        '|40|_|%E2%80%A2',
+        new google.maps.Size(21, 34),
+        new google.maps.Point(0, 0),
+        new google.maps.Point(10, 34),
+        new google.maps.Size(21, 34));
+    return markerImage;
+}
